5ba67e9fbd516b2d5c821a44ebcc644bb5748852
eduardoreyesr <eduardoreyesr@outlook.com>
2025-07-24 16:11:32 -0500

    v 1.0.1

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+  const inProduction = process.env.NODE_ENV = 'production';
+  const isDev = process.env.NODE_ENV = 'development';
 const nextConfig = {
+  
+    basePath: inProduction? '/webkit-master': '',
     output: 'export',
     distDir: 'dist',
     images: {
